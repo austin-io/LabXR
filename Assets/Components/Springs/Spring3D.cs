3ad500a77a570488cd778ee0ad24f66300dbaa84
austin <apoberezhnik23@gmail.com>
2023-05-05 19:38:57 -0700

    Add springs

---
+++ Assets/Components/Springs/Spring3D.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>Creates a spring object based on Vector3 values. Can be configured with frequency (the force multiplier) and damping (the velocity multiplier, aka friction) settings.</summary>
+public class Spring3D : MonoBehaviour {
+
+    [SerializeField] Vector3 _position, _target;
+    [SerializeField] float _frequency, _damping;
+
+    Vector3 _velocity = Vector3.zero;
+
+    ///<summary>Update the spring values</summary>
+    void Update() {
+        Vector3 force = _frequency * (_target - _position);
+        _velocity += force;
+        _position += _velocity;
+        _velocity *= _damping;
+    }
+
+    ///<summary>Stops the spring by setting position, target, and velocity to Vector3.zero.</summary>
+    ///<params></params>
+    ///<returns></returns>
+    public void Stop() {
+        StopAt(Vector3.zero);
+    }
+
+    ///<summary>Stops the spring by setting velocity to Vector3.zero. Position and target gets set to newTarget.</summary>
+    ///<params name="newTarget">The new target and position to set immediately.</params>
+    ///<returns></returns>
+    public void StopAt(Vector3 newTarget) { 
+        _target = newTarget;
+        _position = _target;
+        _velocity = Vector3.zero;
+    }
+
+    ///<summary>Gets the value of the current spring position.</summary>
+    ///<returns>The current spring position.</returns>
+    public Vector3 GetValue() {
+        return _position;
+    }
+
+    ///<summary>Stops the spring by setting velocity to Vector3.zero. Position and target gets set to newTarget.</summary>
+    ///<params name="newTarget">The new target to set immediately.</params>
+    ///<returns></returns>
+    public void SetTarget(Vector3 newTarget) { 
+        _target = newTarget;
+    }
+
+    ///<summary>Sets the current velocity</summary>
+    ///<params name="newVelocity">The new velocity to set immediately.</params>
+    ///<returns></returns>
+    public void Shake(Vector3 newVelocity) {
+        _velocity = newVelocity;
+    }
+}
